e5d26d220f6096a676961b521c8a745028a3ec7b
Sidharth Juyal <sid@whackylabs.com>
2013-03-17 11:29:14 +0530

    article 02: Texture port to iOS

---
+++ ios/02_textures/source/ios_specific/WLViewController.h
@@ -0,0 +1,14 @@
+//
+//  WLViewController.h
+//  01_project_skeleton
+//
+//  Created by Sid on 17/03/13.
+//  Copyright (c) 2013 whackylabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface WLViewController : GLKViewController
+
+@end
