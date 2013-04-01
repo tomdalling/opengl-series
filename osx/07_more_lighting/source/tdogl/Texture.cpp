9ce0ce9fc48807e5bc9a64db95670aeb2ce31b9f
Tom Dalling <tom.dalling@gmail.com>
2013-04-01 17:01:08 +1100

    OSX article 07 code, first draft

---
+++ osx/07_more_lighting/source/tdogl/Texture.cpp
@@ -0,0 +1,75 @@
+/*
+ tdogl::Texture
+ 
+ Copyright 2012 Thomas Dalling - http://tomdalling.com/
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include "Texture.h"
+#include <stdexcept>
+
+using namespace tdogl;
+
+static GLenum TextureFormatForBitmapFormat(Bitmap::Format format, bool srgb)
+{
+    switch (format) {
+        case Bitmap::Format_Grayscale: return GL_LUMINANCE;
+        case Bitmap::Format_GrayscaleAlpha: return GL_LUMINANCE_ALPHA;
+        case Bitmap::Format_RGB: return (srgb ? GL_SRGB : GL_RGB);
+        case Bitmap::Format_RGBA: return (srgb ? GL_SRGB_ALPHA : GL_RGBA);
+        default: throw std::runtime_error("Unrecognised Bitmap::Format");
+    }
+}
+
+Texture::Texture(const Bitmap& bitmap, GLint minMagFiler, GLint wrapMode) :
+    _originalWidth((GLfloat)bitmap.width()),
+    _originalHeight((GLfloat)bitmap.height())
+{
+    glGenTextures(1, &_object);
+    glBindTexture(GL_TEXTURE_2D, _object);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minMagFiler);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, minMagFiler);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0, 
+                 TextureFormatForBitmapFormat(bitmap.format(), true),
+                 (GLsizei)bitmap.width(), 
+                 (GLsizei)bitmap.height(),
+                 0, 
+                 TextureFormatForBitmapFormat(bitmap.format(), false),
+                 GL_UNSIGNED_BYTE, 
+                 bitmap.pixelBuffer());
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+Texture::~Texture()
+{
+    glDeleteTextures(1, &_object);
+}
+
+GLuint Texture::object() const
+{
+    return _object;
+}
+
+GLfloat Texture::originalWidth() const
+{
+    return _originalWidth;
+}
+
+GLfloat Texture::originalHeight() const
+{
+    return _originalHeight;
+}
