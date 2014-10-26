84b933d977251866fcac3bbeed477782036d7948
Tom Dalling <tom@tomdalling.com>
2014-10-26 14:45:22 +1100

    Big restructuring of files
    The OSX projects compile

---
+++ platforms/windows/platform_windows.cpp
@@ -0,0 +1,12 @@
+#include "platform.hpp"
+#include <windows.h>
+
+static std::string ResourcePath(std::string fileName) {
+    char executablePath[1024] = {'\0'};
+    DWORD charsCopied = GetModuleFileName(NULL, executablePath, 1024);
+    if(charsCopied > 0 && charsCopied < 1024)
+        return std::string(executablePath) + "\\..\\" + fileName;
+    else
+        throw std::runtime_error("GetModuleFileName failed a bit");
+}
+
