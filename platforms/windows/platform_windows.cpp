26d67836244d9d2440129cb44747521d8c72a47d
Tom Dalling <tom@tomdalling.com>
2014-10-26 19:53:04 +1100

    01_project_skeleton builds on windows

--- platforms/windows/platform_windows.cpp
+++ platforms/windows/platform_windows.cpp
@@ -1,7 +1,7 @@
 #include "platform.hpp"
 #include <windows.h>
 
-static std::string ResourcePath(std::string fileName) {
+std::string ResourcePath(std::string fileName) {
     char executablePath[1024] = {'\0'};
     DWORD charsCopied = GetModuleFileName(NULL, executablePath, 1024);
     if(charsCopied > 0 && charsCopied < 1024)
