637c7587c1265f3ae86faaf716f9571ba4f5ccd7
Martín Lucas Golini <spartanj@gmail.com>
2015-05-13 00:10:06 -0300

    Updated Linux project files to work with the new project structure, removed all the old repeated files. Also added article 08 "Even more lighting".

---
+++ platforms/linux/platform_linux.cpp
@@ -0,0 +1,100 @@
+#include "platform.hpp"
+#include <string>
+#include <cmath>
+#include <climits>
+#include <limits>
+
+#if defined( __APPLE_CC__ ) || defined ( __APPLE__ )
+	#define PLATFORM_OSX
+	#include <CoreFoundation/CoreFoundation.h>
+#elif defined( __WIN32__ ) || defined( _WIN32 ) || defined( _WIN64 )
+	#define PLATFORM_WIN32
+	#include <windows.h>
+#elif defined ( linux ) || defined( __linux__ )
+	#define PLATFORM_LINUX
+	#include <libgen.h>
+	#include <unistd.h>
+#elif defined( __HAIKU__ ) || defined( __BEOS__ )
+	#define PLATFORM_HAIKU
+	#include <kernel/OS.h>
+	#include <kernel/image.h>
+#elif defined( __SVR4 )
+	#define PLATFORM_SOLARIS
+	#include <stdlib.h>
+#elif defined( __FreeBSD__ ) || defined(__OpenBSD__) || defined( __NetBSD__ ) || defined( __DragonFly__ )
+	#define PLATFORM_BSD
+	#include <sys/sysctl.h>
+#endif
+
+static std::string GetProcessPath() {
+#if defined( PLATFORM_OSX )
+	char exe_file[PATH_MAX + 1];
+	CFBundleRef mainBundle = CFBundleGetMainBundle();
+	if (mainBundle) {
+		CFURLRef mainURL = CFBundleCopyBundleURL(mainBundle);
+
+		if (mainURL) {
+			int ok = CFURLGetFileSystemRepresentation ( mainURL, (Boolean) true, (UInt8*)exe_file, PATH_MAX );
+
+			if (ok) {
+				return std::string(exe_file) + "/";
+			}
+		}
+	}
+
+	return "./";
+#elif defined( PLATFORM_LINUX )
+	char exe_file[PATH_MAX + 1];
+	int size;
+	size = readlink("/proc/self/exe", exe_file, PATH_MAX);
+	if (size < 0) {
+		return "./";
+	} else {
+		exe_file[size] = '\0';
+		return std::string(dirname(exe_file)) + "/";
+	}
+#elif defined( PLATFORM_WIN32 )
+	// Get path to executable:
+	TCHAR szDllName[_MAX_PATH];
+	TCHAR szDrive[_MAX_DRIVE];
+	TCHAR szDir[_MAX_DIR];
+	TCHAR szFilename[_MAX_DIR];
+	TCHAR szExt[_MAX_DIR];
+	GetModuleFileName(0, szDllName, _MAX_PATH);
+
+	_splitpath(szDllName, szDrive, szDir, szFilename, szExt);
+
+	return std::string(szDrive) + std::string(szDir);
+#elif defined( PLATFORM_BSD )
+	int mib[4];
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_PROC;
+	mib[2] = KERN_PROC_PATHNAME;
+	mib[3] = -1;
+	char buf[1024];
+	size_t cb = sizeof(buf);
+	sysctl(mib, 4, buf, &cb, NULL, 0);
+
+	return FileRemoveFileName( std::string( buf ) );
+#elif defined( PLATFORM_SOLARIS )
+	return FileRemoveFileName( std::string( getexecname() ) );
+#elif defined( PLATFORM_HAIKU )
+	image_info info;
+	int32 cookie = 0;
+
+	while ( B_OK == get_next_image_info( 0, &cookie, &info ) ) {
+		if ( info.type == B_APP_IMAGE )
+			break;
+	}
+
+	return FileRemoveFileName( std::string( info.name ) );
+#else
+	#warning GetProcessPath() not implemented on this platform. ( will return "./" )
+	return "./";
+#endif
+}
+
+std::string ResourcePath(std::string fileName) {
+	return GetProcessPath() + "/resources/" + fileName;
+}
+
---
+++ platforms/linux/premake4.lua
@@ -0,0 +1,54 @@
+function create_project( name )
+	project( name .. "-app" )
+		kind "WindowedApp"
+		language "C++"
+		files { "../../source/" .. name .. "/source/**.cpp", "platform_linux.cpp" }
+		targetdir("../../source/" .. name .. "/")
+		includedirs( "../../source/common/" )
+		includedirs( "../../source/common/thirdparty/glm" )
+		includedirs( "../../source/common/thirdparty/stb_image" )
+		
+		configuration "windows"
+			links {"glu32", "opengl32", "gdi32", "winmm", "user32","GLEW"}
+
+		configuration "linux"
+			links {"GL","glfw","GLEW"}
+		
+		configuration "macosx"
+			links {"GL","glfw","GLEW", "CoreFoundation.framework"}
+			libdirs {"/opt/local/lib"}
+			includedirs {"/opt/local/include"}
+		
+		configuration "haiku"
+			links {"GL","glfw","GLEW"}
+
+		configuration "freebsd"
+			links {"GL","glfw","GLEW"}
+		
+		configuration "debug"
+			defines { "DEBUG" }
+			flags { "Symbols" }
+			buildoptions{ "-Wall" }
+			targetname ( name .. "-debug" )
+
+		configuration "release"
+			defines { "NDEBUG" }
+			flags { "Optimize" }
+			buildoptions{ "-Wall" }
+			targetname ( name .. "-release" )
+end
+
+solution "opengl-series"
+	location("./")
+	targetdir("./bin")
+	configurations { "debug", "release" }
+	objdir("obj/" .. os.get() .. "/")
+	
+	create_project( "01_project_skeleton" );
+	create_project( "02_textures" );
+	create_project( "03_matrices" );
+	create_project( "04_camera" );
+	create_project( "05_asset_instance" );
+	create_project( "06_diffuse_lighting" );
+	create_project( "07_more_lighting" );
+	create_project( "08_even_more_lighting" );
